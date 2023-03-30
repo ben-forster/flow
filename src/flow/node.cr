f161c0e6907c949152fb33a256bd85e9ee0db952
Ben Forster <109809772+ben-forster@users.noreply.github.com>
2023-03-30 10:15:17 +0100

    Update Shard & add Node class

---
+++ src/flow/node.cr
@@ -0,0 +1,14 @@
+require "flow/model"
+require "flow/player"
+require "flow/websocket"
+require "event_emitter"
+require "net/http"
+
+module Caldera
+  class Node
+
+    LOGGER = ::Log.for(self)
+
+    
+  end
+end
